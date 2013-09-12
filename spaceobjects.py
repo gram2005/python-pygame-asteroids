636f56ec81853763bc3ebd30bcd95a7c0f7397e8
gram2005 <gram2005@gmail.com>
2013-09-12 21:36:42 +0300

    updated Base_image class
    updated parent Base_image class to include update() method

--- spaceobjects.py
+++ spaceobjects.py
@@ -14,6 +14,25 @@ class Base_image():
         self.angle_speed = angle_speed
         self.position = [x, y]
         
+    def update(self, alt = False):
+        
+        self.angle += self.angle_speed
+        self.x += self.v_x
+        self.y -= self.v_y        
+
+        if 0 > self.x or self.x > 800:
+            self.x = self.x % 800
+        if 0 > self.y or self.y > 600:
+                self.y = self.y % 600      
+
+        if alt:        
+            self.display_image = pygame.transform.rotozoom(self.image_alt, self.angle, 1)
+        else:
+            self.display_image = pygame.transform.rotozoom(self.image, self.angle, 1)
+            
+        self.obj_center = self.display_image.get_rect().center
+        self.position = [self.x - self.obj_center[0], self.y - self.obj_center[1]]    
+        
     def draw(self, screen):
         screen.blit(self.display_image, self.position)
         
@@ -24,10 +43,12 @@ class Ship(Base_image):
         self.acc = 0
         self.moving = False
         Base_image.__init__(self, image, x, y)
-        #self.display_image = self.image
         
     def set_angle_speed(self, value):
         self.angle_speed = value
+        
+    def get_angle(self):
+        return self.angle
         
     def set_acc(self, value):
         self.acc = value
@@ -36,62 +57,26 @@ class Ship(Base_image):
         self.moving = value
         
     def update(self):
-        self.angle += self.angle_speed
         self.v_x = self.acc * math.cos(math.radians(self.angle))
         self.v_y = self.acc * math.sin(math.radians(self.angle))
-        self.x += self.v_x
-        self.y -= self.v_y
         
-        # baisiai nepatinka bagroundo matmenys uzhardkodint :/
-        
-        if 0 > self.x or self.x > 800:
-            self.x = self.x % 800
-        if 0 > self.y or self.y > 600:
-                self.y = self.y % 600 
-        self.position = [self.x, self.y]
+        if not self.moving:
+            Base_image.update(self)
+        else:
+            Base_image.update(self, True)        
                 
         if not self.moving and self.acc > 0:
             self.acc -= 1.5/60
         elif self.acc < 0:
             self.acc = 0
         
-        # galima ir geriau
-        if not self.moving:
-            self.display_image = pygame.transform.rotozoom(self.image, self.angle, 1)
-        else:
-            self.display_image = pygame.transform.rotozoom(self.image_alt, self.angle, 1)
-            
-        self.ship_center = self.display_image.get_rect().center
-        
-        self.position = [self.x - self.ship_center[0], self.y - self.ship_center[1]]
-        
-        #print (self.position, self.ship_center, self.v_x, self.v_y, self.moving)
-        
-        
+       
 class Asteroid (Base_image):
     def __init__(self, image):
         x = random.randrange(0, 800)
         y = random.randrange(0, 600)
-        v_x = random.random()*10 - 5
-        v_y = random.random()*10 - 5
+        v_x = random.random()*6 - 3
+        v_y = random.random()*6 - 3
         angle = random.randrange(0, 360)
         angle_speed = random.randrange(-10, 10)
         Base_image.__init__(self, image, x, y, v_x, v_y, angle, angle_speed)
-        
-    def update(self):
-        
-        self.angle += self.angle_speed
-        self.x += self.v_x
-        self.y -= self.v_y        
-
-
-        if 0 > self.x or self.x > 800:
-            self.x = self.x % 800
-        if 0 > self.y or self.y > 600:
-                self.y = self.y % 600      
-
-        #print(self.angle, self.angle_speed)        
-                
-        self.display_image = pygame.transform.rotozoom(self.image, self.angle, 1)        
-        self.aster_center = self.display_image.get_rect().center
-        self.position = [self.x - self.aster_center[0], self.y - self.aster_center[1]]
