058a233171d1c100dfc509f0519278ce554d514f
gram2005 <gram2005@gmail.com>
2013-12-29 16:26:22 +0200

    added start screen

--- game.py
+++ game.py
@@ -25,18 +25,11 @@ max_num_of_ateroids = 10
 asteroid_list = []
 missile_list = []
 ateroid_timer = 0
+game_state = 'not started'
 done = False
 
 #images
-bg = Base_image("bg_nebula_blue.png")
-ship = Ship("ship1.png", "ship2.png", 400, 300)
-for x in range(max_num_of_ateroids):
-    asteroid = Asteroid("asteroid_blue.png")
-    asteroid_list.append(asteroid)
-
-
-
-
+background = Base_image("bg_nebula_blue.png")
 
 ################################################################################    
 while done == False:
@@ -45,104 +38,129 @@ while done == False:
         if event.type == pygame.QUIT:
             done = True
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                ship.set_angle_speed(5)
-                ship.switch_state(True)
-            if event.key == pygame.K_RIGHT:
-                ship.set_angle_speed(-5)
-                ship.switch_state(True)
-            if event.key == pygame.K_UP:
-                ship.set_acc(5)
-                ship.switch_state(True)
-            if event.key == pygame.K_SPACE:
-                x, y = ship.get_missile_coord()
-                v_x, v_y = ship.get_missile_speed()
-                missile_list.append(Missile("shot2.png", x, y, v_x, v_y))
+        if game_state == 'not started':
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                game_state = 'started'
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                ship.set_angle_speed(0)
-                ship.switch_state(False)
-            if event.key == pygame.K_RIGHT:
-                ship.set_angle_speed(0)
-                ship.switch_state(False)
-            if event.key == pygame.K_UP:
-                ship.switch_state(False)
+        if game_state == 'running':
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    ship.set_angle_speed(5)
+                    ship.switch_state(True)
+                if event.key == pygame.K_RIGHT:
+                    ship.set_angle_speed(-5)
+                    ship.switch_state(True)
+                if event.key == pygame.K_UP:
+                    ship.set_acc(5)
+                    ship.switch_state(True)
+                if event.key == pygame.K_SPACE:
+                    x, y = ship.get_missile_coord()
+                    v_x, v_y = ship.get_missile_speed()
+                    missile_list.append(Missile("shot2.png", x, y, v_x, v_y))
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    ship.set_angle_speed(0)
+                    ship.switch_state(False)
+                if event.key == pygame.K_RIGHT:
+                    ship.set_angle_speed(0)
+                    ship.switch_state(False)
+                if event.key == pygame.K_UP:
+                    ship.switch_state(False)
                 
 ################################################################################        
     # game logic
 
-    rem_asteroid_list = []
-    rem_missile_list = []
-    for asteroid in asteroid_list:
-        for missile in missile_list:
-            if missile.check_collision(asteroid):
-                rem_missile_list.append(missile)
+    if game_state == 'not started':
+        start_text1= basicFont.render('PRESS ENTER', True, white)
+        start_text2= basicFont.render('TO START THE GAME', True, white)
+
+    if game_state == 'started':
+
+        ship = Ship("ship1.png", "ship2.png", 400, 300)
+        for x in range(max_num_of_ateroids):
+            asteroid = Asteroid("asteroid_blue.png")
+            asteroid_list.append(asteroid)
+        game_state = 'running'
+
+    if game_state == 'running':
+        rem_asteroid_list = []
+        rem_missile_list = []
+        for asteroid in asteroid_list:
+            for missile in missile_list:
+                if missile.check_collision(asteroid):
+                    rem_missile_list.append(missile)
+                    rem_asteroid_list.append(asteroid)
+                    score += 1010
+
+            if ship.check_collision(asteroid):
                 rem_asteroid_list.append(asteroid)
-                score += 1010
+                num_of_lifes -= 1
 
-        if ship.check_collision(asteroid):
-            rem_asteroid_list.append(asteroid)
-            num_of_lifes -= 1
-            
-    for missile in rem_missile_list:
-        try:
-            missile_list.remove(missile)
-        except:
-            pass
+        for missile in rem_missile_list:
+            try:
+                missile_list.remove(missile)
+            except:
+                pass
 
-    for asteroid in rem_asteroid_list:
-        try:
-            asteroid_list.remove(asteroid)
-        except:
-            pass
-        
-    rem_missile_list = []
-    for missile in missile_list:
-        if missile.is_alive():
-            missile.update()
-        else:
-            rem_missile_list.append(missile)
+        for asteroid in rem_asteroid_list:
+            try:
+                asteroid_list.remove(asteroid)
+            except:
+                pass
 
-    for missile in rem_missile_list:
-        try:
-            missile_list.remove(missile)
-        except:
-            pass
-        
-    ship.update()
+        rem_missile_list = []
+        for missile in missile_list:
+            if missile.is_alive():
+                missile.update()
+            else:
+                rem_missile_list.append(missile)
 
-    if len(asteroid_list) < max_num_of_ateroids:
-        ateroid_timer += 1
+        for missile in rem_missile_list:
+            try:
+                missile_list.remove(missile)
+            except:
+                pass
 
-    if ateroid_timer == 60:
-        asteroid = Asteroid("asteroid_blue.png")
-        asteroid_list.append(asteroid)
-        ateroid_timer = 0
-    
-    for asteroid in asteroid_list:
-        asteroid.update()
+        ship.update()
 
-    life_text = basicFont.render('Lifes: ' + str(num_of_lifes), True, white)
-    score_text = basicFont.render('Score: ' + str(score), True, white)
+        if len(asteroid_list) < max_num_of_ateroids:
+            ateroid_timer += 1
+
+        if ateroid_timer == 60:
+            asteroid = Asteroid("asteroid_blue.png")
+            asteroid_list.append(asteroid)
+            ateroid_timer = 0
+
+        for asteroid in asteroid_list:
+            asteroid.update()
+
+        life_text = basicFont.render('Lifes: ' + str(num_of_lifes), True, white)
+        score_text = basicFont.render('Score: ' + str(score), True, white)
 
 
 ################################################################################    
     # drawing
     screen.fill(white)
     
-    bg.draw(screen)
-    ship.draw(screen)
-    
-    for asteroid in asteroid_list:
-        asteroid.draw(screen)
-        
-    for missile in missile_list:
-            missile.draw(screen)
+    background.draw(screen)
 
-    screen.blit(life_text, (688,10))
-    screen.blit(score_text, (10,10))
+    if game_state == 'not started':
+        pygame.draw.rect(screen, white, (200,100,400,400), 10)
+        screen.blit(start_text1, (295,250))
+        screen.blit(start_text2, (250,330))
+
+    if game_state == 'running':
+        ship.draw(screen)
+    
+        for asteroid in asteroid_list:
+            asteroid.draw(screen)
+
+        for missile in missile_list:
+                missile.draw(screen)
+
+        screen.blit(life_text, (688,10))
+        screen.blit(score_text, (10,10))
 
     pygame.display.flip()
 
